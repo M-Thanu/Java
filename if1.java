accc78176113ff37a063b53f7954e0ed4cf929d5
M-Thanu <mahanthanuja@gmail.com>
2025-08-02 16:20:22 +0530

    ifelse

---
+++ if1.java
@@ -0,0 +1,10 @@
+class if1{
+    public static void main(String Args[]){
+        boolean rain=true;
+        if(rain){
+            System.out.print("Umbrella");
+        }else{
+            System.out.print("No");
+        }
+    }
+}
