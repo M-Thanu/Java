706407446d4a5ecc6e0d40c47a72077449554fab
M-Thanu <mahanthanuja@gmail.com>
2025-08-03 18:54:37 +0530

    ternery

--- if1.java
+++ if1.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 class if1{
     public static void main(String Args[]){
         boolean rain=true;
-        if(rain){
+        /*if(rain){
             System.out.print("Umbrella");
         }else{
             System.out.print("No");
-        }
+        }*/
+
+       /*Scanner bin = new Scanner(System.in);
+       System.out.print("Enter Score:");
+       int score = bin.nextInt();
+
+       if(score<50){
+        System.out.print("Need to improve");
+       }else if(score == 50 || score < 75){
+        System.out.print("Good Job");
+       }else{
+        System.out.print("Excellent");
+       }*/
+      System.out.print(rain?"Umbrella":"Sunlight");
     }
 }
